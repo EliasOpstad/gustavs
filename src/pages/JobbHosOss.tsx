79805353993c748c19de804d989f21bdb1c2dc86
eliasgo <you@example.com>
2023-09-25 14:35:04 +0200

    Merge fixed.

--- src/pages/JobbHosOss.tsx
+++ src/pages/JobbHosOss.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import image from "../Images/soknad.png";
 import Soknad from "../CommonComponents/Soknad";
 
 const Container = styled.div`
